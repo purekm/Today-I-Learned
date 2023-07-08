df4ff7fb965572ab37fbdd0ea189266ebbb6e7cd
purekm <rudals4349@naver.com>
2023-07-08 22:56:03 +0900

    재귀(하노이 탑)

--- DataStructure/Day4/Day4.c
+++ DataStructure/Day4/Day4.c
@@ -4,7 +4,8 @@ void HanoiTowerMove(int num, char from, char by, char to)
     if(num == 1){
         printf("원반1을 %c 에서 %c로 이동\n",from,to);
     }
-    else{
+    else
+    {
         HanoiTowerMove(num-1,from,to,by);
         printf("원반%d를 %c 에서 %c로 이동\n",num,from,to);
         HanoiTowerMove(num-1,by,from,to);
