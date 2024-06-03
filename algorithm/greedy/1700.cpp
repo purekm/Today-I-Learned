976db711669614eb6643e2abc60ce3ba26865dae
purekm <rudals4349@naver.com>
2024-06-03 16:32:17 +0900

    optimal 알고리즘

--- algorithm/greedy/1700.cpp
+++ algorithm/greedy/1700.cpp
@@ -4,27 +4,52 @@
 #include <vector>
 
 using namespace std;
-set<int> plugin;
 
 int main(){
-    vector<int> vec;
     int arr[101];
     int hole,n;
+    int cnt = 0;
     cin >> hole >> n;
-    for(int i=1;i<=n;i++){
-        int a;
-        cin >> a;
-        if(i<=hole) {
-            plugin.insert(a);
-            continue;
+    int multitap[101] ={0,};
+    for(int i=0;i<n;i++) cin >> arr[i];
+    for(int i=0;i<n;i++){
+        bool check = false;
+        for(int j=0;j<hole;j++){ // 이미 멀티탭에 있는 경우
+            if(multitap[j] == arr[i]) {
+                check = true;
+                break;
+            }
         }
-        arr[i] = a;
-        
-    }
-    for(int i=hole;i<=n;i++){
-        plugin.find(arr[i]);
-    }
+        if(check) continue;
 
+        for(int j=0;j<hole;j++){ // 빈칸이 있는 경우
+            if(multitap[j]==0){ //멀티탭에 빈칸이 있는 경우
+                multitap[j] = arr[i];
+                check = true;
+                break;
+            }
+        }
+        if(check) continue;
+
+        int last = -1;
+        int index = -1;
+        for(int j=0;j<hole;j++){ //가장 늦게 쓸 아이템을 빼기
+            int pos = 0;
+            for(int l=i+1;l<n;l++){
+                if(multitap[j]==arr[l]){
+                    break;
+                }
+                pos++;
+            }
+            if(pos>last) {
+                last = pos;
+                index = j;
+            }
+        }
+        multitap[index] = arr[i]; //다음꺼로 변경
+        cnt ++;
+    }
+    cout << cnt;
     
     return 0;
 }
