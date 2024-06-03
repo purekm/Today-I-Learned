961c5213a56092a2eba7f9a29326f9503e4d21c0
purekm <rudals4349@naver.com>
2024-06-03 16:43:56 +0900

    optimal 알고리즘

--- algorithm/greedy/1700.cpp
+++ algorithm/greedy/1700.cpp
@@ -1,20 +1,21 @@
 #include <iostream>
-#include <set>
-#include <algorithm>
 #include <vector>
 
 using namespace std;
 
 int main(){
     int arr[101];
-    int hole,n;
+    int hole, n;
     int cnt = 0;
     cin >> hole >> n;
-    int multitap[101] ={0,};
-    for(int i=0;i<n;i++) cin >> arr[i];
-    for(int i=0;i<n;i++){
+    vector<int> multitap(hole, 0); 
+    for(int i = 0; i < n; i++) cin >> arr[i];
+
+    for(int i = 0; i < n; i++) {
         bool check = false;
-        for(int j=0;j<hole;j++){ // 이미 멀티탭에 있는 경우
+
+        // 이미 멀티탭에 있는 경우
+        for(int j = 0; j < hole; j++) {
             if(multitap[j] == arr[i]) {
                 check = true;
                 break;
@@ -22,8 +23,9 @@ int main(){
         }
         if(check) continue;
 
-        for(int j=0;j<hole;j++){ // 빈칸이 있는 경우
-            if(multitap[j]==0){ //멀티탭에 빈칸이 있는 경우
+        // 빈칸이 있는 경우
+        for(int j = 0; j < hole; j++) {
+            if(multitap[j] == 0) {
                 multitap[j] = arr[i];
                 check = true;
                 break;
@@ -33,21 +35,23 @@ int main(){
 
         int last = -1;
         int index = -1;
-        for(int j=0;j<hole;j++){ //가장 늦게 쓸 아이템을 빼기
-            int pos = 0;
-            for(int l=i+1;l<n;l++){
-                if(multitap[j]==arr[l]){
+
+        // 가장 늦게 사용할 아이템을 찾기
+        for(int j = 0; j < hole; j++) {
+            int pos = n; 
+            for(int l = i + 1; l < n; l++) {
+                if(multitap[j] == arr[l]) {
+                    pos = l;
                     break;
                 }
-                pos++;
             }
-            if(pos>last) {
+            if(pos > last) {
                 last = pos;
                 index = j;
             }
         }
-        multitap[index] = arr[i]; //다음꺼로 변경
-        cnt ++;
+        multitap[index] = arr[i];
+        cnt++;
     }
     cout << cnt;
     
